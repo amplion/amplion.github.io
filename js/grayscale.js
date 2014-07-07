26ccee201de4c84917c949e7d35c9ab7fa1682eb
Chris Kraybill <ckraybill@gmail.com>
2014-07-07 16:42:15 -0700

    New font, background image, proper markup

--- js/grayscale.js
+++ js/grayscale.js
@@ -21,7 +21,7 @@ $(function() {
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
     zoom: 15,
-    center: new google.maps.LatLng(53.385873, -1.471471),
+    center: new google.maps.LatLng(44.043083, -121.328302),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
     styles: [{
